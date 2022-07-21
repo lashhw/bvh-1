72fa2164a825b4036ccb576732a53f11482ac403
Jacky Huang <lashhw81@gmail.com>
2022-07-21 20:07:02 +0800

    hello bunny :)

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <array>
 #include <vector>
 #include <cmath>
+#include <fstream>
 #include <cfloat>
 #include <numeric>
 
@@ -13,23 +14,61 @@
 #include "triangle.hpp"
 #include "aabb_intersector.hpp"
 #include "bvh.hpp"
+#include "happly.h"
 
 int main() {
+    happly::PLYData ply_in("../bun_zipper.ply");
+    std::vector<std::array<double, 3>> v_pos = ply_in.getVertexPositions();
+    std::vector<std::vector<size_t>> f_index = ply_in.getFaceIndices<size_t>();
+
     std::vector<Triangle> triangles;
-    triangles.emplace_back(Vec3( 0,  0, 0), Vec3( 1,  2,  3), Vec3( 0,  1,  2));
-    triangles.emplace_back(Vec3( 2,  1, 0), Vec3( 0,  2,  1), Vec3( 2,  4,  1));
-    triangles.emplace_back(Vec3( 2, -4, 1), Vec3( 1, -3, -1), Vec3( 4, -4,  0));
+    for (const auto &face : f_index) {
+        triangles.emplace_back(Vec3(v_pos[face[0]][0], v_pos[face[0]][1], v_pos[face[0]][2]),
+                               Vec3(v_pos[face[1]][0], v_pos[face[1]][1], v_pos[face[1]][2]),
+                               Vec3(v_pos[face[2]][0], v_pos[face[2]][1], v_pos[face[2]][2]));
+    }
 
+    std::cout << "Building BVH... " << std::flush;
     Bvh bvh(&triangles);
+    std::cout << "done" << std::endl;
 
-    Vec3 origin(3, -5, 0);
-    Vec3 direction(-2, 9, 1);
-    Ray ray(origin, direction);
-    Triangle::Intersection intersection;
-    bool hit = bvh.traverse(ray, intersection);
+    Vec3 origin(0, 0, 1);
+    Vec3 upper_left_corner(-0.12, 0.2, 0);
+    Vec3 horizontal(0.2, 0, 0);
+    Vec3 vertical(0, -0.2, 0);
 
-    if (hit) {
-        Vec3 hit_point = origin + intersection.t * direction;
-        std::cout << "Hit: " << hit_point.x << ' ' << hit_point.y << ' ' << hit_point.z << std::endl;
+    float min_t = FLT_MAX;
+    float max_t = 0;
+    float t[800][800];
+    bool miss[800][800];
+    for (int j = 0; j < 800; j++) {
+        for (int i = 0; i < 800; i++) {
+            Vec3 lookat = upper_left_corner + horizontal * (i / 800.0f) + vertical * (j / 800.0f);
+            Ray ray(origin, lookat - origin);
+
+            Triangle::Intersection intersection;
+            bool hit = bvh.traverse(ray, intersection);
+            if (hit) {
+                min_t = std::min(min_t, intersection.t);
+                max_t = std::max(max_t, intersection.t);
+                t[i][j] = intersection.t;
+                miss[i][j] = false;
+            }
+            else miss[i][j] = true;
+        }
+    }
+
+    std::ofstream file("../image.ppm");
+    file << "P3\n800 800\n255\n";
+    for (int j = 0; j < 800; j++) {
+        for (int i = 0; i < 800; i++) {
+            if (miss[i][j]) file << "0 0 0 ";
+            else {
+                float normalized_t = (t[i][j] - min_t) / (max_t - min_t);
+                float color = std::sqrt(1 - normalized_t);
+                int r = std::min(255, int(color * 255));
+                file << r << " " << r << " " << r << " ";
+            }
+        }
     }
 }
